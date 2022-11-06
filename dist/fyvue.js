6f3563e447eb62ff9c4f06059f570edc24400922
Florian Gasquez <florian@fy.to>
2022-11-06 16:56:39 +0100

    hm... :)

---
+++ dist/fyvue.js
@@ -0,0 +1,132 @@
+'use strict';
+
+var head$1 = require('@vueuse/head');
+var vue = require('vue');
+var vue$1 = require('@headlessui/vue');
+var solid = require('@heroicons/vue/24/solid');
+
+function mitt(n){return {all:n=n||new Map,on:function(t,e){var i=n.get(t);i?i.push(e):n.set(t,[e]);},off:function(t,e){var i=n.get(t);i&&(e?i.splice(i.indexOf(e)>>>0,1):n.set(t,[]));},emit:function(t,e){var i=n.get(t);i&&i.slice().map(function(n){n(e);}),(i=n.get("*"))&&i.slice().map(function(n){n(t,e);});}}}
+
+const eventBus = mitt();
+const useEventBus = () => {
+    const vueInstance = vue.getCurrentInstance();
+    return vueInstance === null || vueInstance === void 0 ? void 0 : vueInstance.appContext.config.globalProperties.$eventBus;
+};
+
+const _hoisted_1 = { class: "parent" };
+var script = /*#__PURE__*/ vue.defineComponent({
+    __name: 'FyModal',
+    props: {
+        id: { type: String, default: "CustomModal" },
+        title: { type: String, default: "" },
+        onOpen: { type: Function, default: () => { } },
+        onClose: { type: Function, default: () => { } },
+        closeIcon: { type: Object, default: solid.XCircleIcon },
+    },
+    setup(__props) {
+        const props = __props;
+        const eventBus = useEventBus();
+        const isOpen = vue.ref(false);
+        const setModal = (value) => {
+            if (value === true)
+                props.onOpen();
+            else {
+                props.onClose();
+            }
+            isOpen.value = Boolean(value);
+        };
+        vue.onMounted(() => {
+            eventBus.on(`${props.id}Modal`, setModal);
+        });
+        vue.onUnmounted(() => {
+            eventBus.off(`${props.id}Modal`, setModal);
+        });
+        return (_ctx, _cache) => {
+            return (vue.openBlock(), vue.createBlock(vue.unref(vue$1.TransitionRoot), {
+                show: isOpen.value,
+                as: "template",
+                enter: "duration-300 ease-out",
+                "enter-from": "opacity-0",
+                "enter-to": "opacity-100",
+                leave: "duration-200 ease-in",
+                "leave-from": "opacity-100",
+                "leave-to": "opacity-0"
+            }, {
+                default: vue.withCtx(() => [
+                    vue.createVNode(vue.unref(vue$1.Dialog), {
+                        open: isOpen.value,
+                        onClose: setModal,
+                        style: { "background": "rgba(0, 0, 0, 0.8)" },
+                        class: "fy-modal"
+                    }, {
+                        default: vue.withCtx(() => [
+                            vue.createElementVNode("div", _hoisted_1, [
+                                vue.createVNode(vue.unref(vue$1.DialogPanel), { class: "modal-container" }, {
+                                    default: vue.withCtx(() => [
+                                        (__props.title)
+                                            ? (vue.openBlock(), vue.createBlock(vue.unref(vue$1.DialogTitle), {
+                                                key: 0,
+                                                class: "title"
+                                            }, {
+                                                default: vue.withCtx(() => [
+                                                    vue.createTextVNode(vue.toDisplayString(__props.title) + " ", 1 /* TEXT */),
+                                                    vue.createElementVNode("a", {
+                                                        href: "javascript:void(0)",
+                                                        onClick: _cache[0] || (_cache[0] = ($event) => (setModal(false)))
+                                                    }, [
+                                                        (vue.openBlock(), vue.createBlock(vue.resolveDynamicComponent(__props.closeIcon), { class: "close-icon" }))
+                                                    ])
+                                                ]),
+                                                _: 1 /* STABLE */
+                                            }))
+                                            : (vue.openBlock(), vue.createElementBlock("a", {
+                                                key: 1,
+                                                href: "javascript:void(0)",
+                                                onClick: _cache[1] || (_cache[1] = ($event) => (setModal(false)))
+                                            }, [
+                                                (vue.openBlock(), vue.createBlock(vue.resolveDynamicComponent(__props.closeIcon), { class: "close-icon is-alone" }))
+                                            ])),
+                                        vue.createElementVNode("div", {
+                                            class: vue.normalizeClass(!__props.title ? 'is-alone modal-content' : 'modal-content')
+                                        }, [
+                                            vue.renderSlot(_ctx.$slots, "default")
+                                        ], 2 /* CLASS */)
+                                    ]),
+                                    _: 3 /* FORWARDED */
+                                })
+                            ])
+                        ]),
+                        _: 3 /* FORWARDED */
+                    }, 8 /* PROPS */, ["open"])
+                ]),
+                _: 3 /* FORWARDED */
+            }, 8 /* PROPS */, ["show"]));
+        };
+    }
+});
+
+script.__file = "src/components/ui/FyModal/FyModal.vue";
+
+var uiComponents = /*#__PURE__*/Object.freeze({
+    __proto__: null,
+    FyModal: script
+});
+
+const head = head$1.createHead();
+const createFyvue = () => {
+    const install = (app, options) => {
+        app.use(head);
+        app.config.globalProperties.$eventBus = eventBus;
+        // Loading UI components
+        for (const componentKey in uiComponents) {
+            app.component(componentKey, uiComponents[componentKey]);
+        }
+    };
+    return {
+        install
+    };
+};
+
+exports.createFyvue = createFyvue;
+exports.useEventBus = useEventBus;
+//# sourceMappingURL=fyvue.js.map
---
+++ dist/fyvue.mjs
@@ -0,0 +1,129 @@
+import { createHead } from '@vueuse/head';
+import { getCurrentInstance, defineComponent, ref, onMounted, onUnmounted, openBlock, createBlock, unref, withCtx, createVNode, createElementVNode, createTextVNode, toDisplayString, resolveDynamicComponent, createElementBlock, normalizeClass, renderSlot } from 'vue';
+import { TransitionRoot, Dialog, DialogPanel, DialogTitle } from '@headlessui/vue';
+import { XCircleIcon } from '@heroicons/vue/24/solid';
+
+function mitt(n){return {all:n=n||new Map,on:function(t,e){var i=n.get(t);i?i.push(e):n.set(t,[e]);},off:function(t,e){var i=n.get(t);i&&(e?i.splice(i.indexOf(e)>>>0,1):n.set(t,[]));},emit:function(t,e){var i=n.get(t);i&&i.slice().map(function(n){n(e);}),(i=n.get("*"))&&i.slice().map(function(n){n(t,e);});}}}
+
+const eventBus = mitt();
+const useEventBus = () => {
+    const vueInstance = getCurrentInstance();
+    return vueInstance === null || vueInstance === void 0 ? void 0 : vueInstance.appContext.config.globalProperties.$eventBus;
+};
+
+const _hoisted_1 = { class: "parent" };
+var script = /*#__PURE__*/ defineComponent({
+    __name: 'FyModal',
+    props: {
+        id: { type: String, default: "CustomModal" },
+        title: { type: String, default: "" },
+        onOpen: { type: Function, default: () => { } },
+        onClose: { type: Function, default: () => { } },
+        closeIcon: { type: Object, default: XCircleIcon },
+    },
+    setup(__props) {
+        const props = __props;
+        const eventBus = useEventBus();
+        const isOpen = ref(false);
+        const setModal = (value) => {
+            if (value === true)
+                props.onOpen();
+            else {
+                props.onClose();
+            }
+            isOpen.value = Boolean(value);
+        };
+        onMounted(() => {
+            eventBus.on(`${props.id}Modal`, setModal);
+        });
+        onUnmounted(() => {
+            eventBus.off(`${props.id}Modal`, setModal);
+        });
+        return (_ctx, _cache) => {
+            return (openBlock(), createBlock(unref(TransitionRoot), {
+                show: isOpen.value,
+                as: "template",
+                enter: "duration-300 ease-out",
+                "enter-from": "opacity-0",
+                "enter-to": "opacity-100",
+                leave: "duration-200 ease-in",
+                "leave-from": "opacity-100",
+                "leave-to": "opacity-0"
+            }, {
+                default: withCtx(() => [
+                    createVNode(unref(Dialog), {
+                        open: isOpen.value,
+                        onClose: setModal,
+                        style: { "background": "rgba(0, 0, 0, 0.8)" },
+                        class: "fy-modal"
+                    }, {
+                        default: withCtx(() => [
+                            createElementVNode("div", _hoisted_1, [
+                                createVNode(unref(DialogPanel), { class: "modal-container" }, {
+                                    default: withCtx(() => [
+                                        (__props.title)
+                                            ? (openBlock(), createBlock(unref(DialogTitle), {
+                                                key: 0,
+                                                class: "title"
+                                            }, {
+                                                default: withCtx(() => [
+                                                    createTextVNode(toDisplayString(__props.title) + " ", 1 /* TEXT */),
+                                                    createElementVNode("a", {
+                                                        href: "javascript:void(0)",
+                                                        onClick: _cache[0] || (_cache[0] = ($event) => (setModal(false)))
+                                                    }, [
+                                                        (openBlock(), createBlock(resolveDynamicComponent(__props.closeIcon), { class: "close-icon" }))
+                                                    ])
+                                                ]),
+                                                _: 1 /* STABLE */
+                                            }))
+                                            : (openBlock(), createElementBlock("a", {
+                                                key: 1,
+                                                href: "javascript:void(0)",
+                                                onClick: _cache[1] || (_cache[1] = ($event) => (setModal(false)))
+                                            }, [
+                                                (openBlock(), createBlock(resolveDynamicComponent(__props.closeIcon), { class: "close-icon is-alone" }))
+                                            ])),
+                                        createElementVNode("div", {
+                                            class: normalizeClass(!__props.title ? 'is-alone modal-content' : 'modal-content')
+                                        }, [
+                                            renderSlot(_ctx.$slots, "default")
+                                        ], 2 /* CLASS */)
+                                    ]),
+                                    _: 3 /* FORWARDED */
+                                })
+                            ])
+                        ]),
+                        _: 3 /* FORWARDED */
+                    }, 8 /* PROPS */, ["open"])
+                ]),
+                _: 3 /* FORWARDED */
+            }, 8 /* PROPS */, ["show"]));
+        };
+    }
+});
+
+script.__file = "src/components/ui/FyModal/FyModal.vue";
+
+var uiComponents = /*#__PURE__*/Object.freeze({
+    __proto__: null,
+    FyModal: script
+});
+
+const head = createHead();
+const createFyvue = () => {
+    const install = (app, options) => {
+        app.use(head);
+        app.config.globalProperties.$eventBus = eventBus;
+        // Loading UI components
+        for (const componentKey in uiComponents) {
+            app.component(componentKey, uiComponents[componentKey]);
+        }
+    };
+    return {
+        install
+    };
+};
+
+export { createFyvue, useEventBus };
+//# sourceMappingURL=fyvue.mjs.map
