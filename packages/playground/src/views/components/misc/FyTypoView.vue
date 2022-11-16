0194ceb1971c2376b34b9be3a24af8296d6529dc
Florian Gasquez <florian@fy.to>
2022-11-16 17:40:29 +0100

    typo

---
+++ packages/playground/src/views/components/misc/FyTypoView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="fv-typo">
+      <h1>This is a h1</h1>
+      <p>This is a p. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec tellus metus. Aenean vel libero blandit lorem rhoncus lacinia. Phasellus feugiat metus sit amet magna vulputate viverra. Nam efficitur dapibus sodales.</p>
+      <h2>This is a h2</h2>
+      <blockquote>What Is Grief, If Not Love Persevering (blockquote)</blockquote>
+      <h3>This is a h3</h3>
+      <ul>
+        <li>
+            This is a list
+        </li>
+        <li>
+            And this is an element
+        </li>
+        <li>
+            Form that list
+        </li>
+      </ul>
+      <h4>This is a h4</h4>
+      <p>This is a p with links. Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit. Nulla nec tellus metus. Aenean <a href="#">vel</a> libero blandit lorem rhoncus lacinia. <a href="#">Phasellus</a> feugiat metus sit amet magna vulputate viverra. Nam efficitur dapibus sodales.</p>
+      <h5>This is a h5</h5>
+      <p>This is a with a hr after. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec tellus metus. Aenean vel libero blandit lorem rhoncus lacinia. Phasellus feugiat metus sit amet magna vulputate viverra. Nam efficitur dapibus sodales.</p>
+      <hr />
+      <p>This is a p. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec tellus metus. Aenean vel libero blandit lorem rhoncus lacinia. Phasellus feugiat metus sit amet magna vulputate viverra. Nam efficitur dapibus sodales.</p>
+
+      <h6>This is a h6</h6>
+  </div>
+</template>
