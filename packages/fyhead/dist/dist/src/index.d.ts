d00c40c773a065229055f2d62368c432f8aaba95
Florian Gasquez <florian@fy.to>
2022-12-03 17:40:39 +0100

    -

--- packages/fyhead/dist/dist/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { FyHead } from './fyhead';
-import type { FyHeadLazy } from './fyhead';
-declare const useFyHead: () => FyHead;
-declare const createFyHead: () => FyHead;
-export { useFyHead, createFyHead, FyHeadLazy };
