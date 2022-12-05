ae171c86bbe0c74a0acaab63f9afd4c6e95b4505
Florian Gasquez <florian@fy.to>
2022-12-05 13:01:59 +0100

    -

--- packages/fyvue/src/components/ui/FyNavbar/FyNavbar.vue
+++ packages/fyvue/src/components/ui/FyNavbar/FyNavbar.vue
@@ -1,21 +1,11 @@
 <script setup lang="ts">
-import { useDark, useToggle } from '@vueuse/core';
-import { MoonIcon, SunIcon, ShoppingCartIcon } from '@heroicons/vue/24/solid';
-import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue';
-import { ref, computed } from 'vue';
+import { ShoppingCartIcon } from '@heroicons/vue/24/solid';
+import { computed } from 'vue';
 import { useFVStore } from '../../../utils/store';
 import { useHistory } from '../../../utils/ssr';
 import type { NavLink } from '../../../dts/index';
 import { ClientOnly } from '../../helpers/ClientOnly';
-const isDark = useDark({
-  selector: 'html',
-  attribute: 'class',
-  valueDark: 'dark',
-  valueLight: 'light',
-});
-const isOpen = ref<boolean>(false);
-const toggleDark = useToggle(isDark);
-const toggleNavbarOpen = useToggle(isOpen);
+import { HeaderNavbar } from '@fy-/ui';
 const store = useFVStore();
 const isAuth = computed(() => store.isAuth);
 const cartCount = computed(() => store.cartCount);
@@ -48,155 +38,56 @@ withDefaults(
 );
 </script>
 <template>
-  <nav class="fy-navbar">
-    <div class="nav-container">
-      <router-link to="/" class="logo-image">
-        <slot name="logo"></slot>
-        <span v-if="title && showTitle">{{ title }}</span>
-      </router-link>
-      <div class="nav-actions">
-        <ClientOnly>
-          <slot name="cart">
-            <template v-if="showCart">
-              <router-link :to="cartPath" class="nav-cart">
-                <ShoppingCartIcon />
-                <div class="badge">{{ cartCount.toString() }}</div>
-              </router-link>
-            </template>
-          </slot>
-        </ClientOnly>
-
-        <button
-          @click="toggleDark()"
-          class="btn neutral light-dark"
-          v-if="darkLight"
-        >
-          <MoonIcon v-if="!isDark" />
-          <SunIcon v-else />
-        </button>
-        <button
-          type="button"
-          class="open-nav-button"
-          @click="toggleNavbarOpen()"
-        >
-          <span class="is-sr">Open main menu</span>
-          <svg
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </button>
-      </div>
-      <div class="nav-menu" :class="isOpen ? 'is-open' : ''">
-        <ul class="main-ul">
-          <li v-for="(link, index) in links" :key="`link_${index.toString()}`">
-            <template v-if="link.childrens && link.childrens.length > 0">
-              <Menu>
-                <MenuButton class="is-link has-childs">
-                  {{ link.name }}
-                  <svg
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                </MenuButton>
-                <transition name="fade">
-                  <MenuItems class="sub-nav">
-                    <ul>
-                      <MenuItem
-                        v-for="(children, index) in link.childrens"
-                        :key="`link_children_${index.toString()}`"
-                      >
-                        <li>
-                          <router-link
-                            v-if="!children.isExternal"
-                            :to="children.to"
-                            :title="children.name"
-                            :alt="children.name"
-                            class="is-link"
-                          >
-                            {{ children.name }}
-                          </router-link>
-                          <a
-                            v-else
-                            :href="children.to"
-                            :title="children.name"
-                            :alt="children.name"
-                            class="is-link"
-                          >
-                            {{ children.name }}
-                          </a>
-                        </li>
-                      </MenuItem>
-                    </ul>
-                  </MenuItems>
-                </transition>
-              </Menu>
-            </template>
-            <template v-else>
-              <router-link
-                v-if="!link.isExternal"
-                :to="link.to"
-                :title="link.name"
-                :alt="link.name"
-                class="is-link"
-                :class="false ? 'is-active' : ''"
-              >
-                {{ link.name }}
-              </router-link>
-              <a
-                v-else
-                :href="link.to"
-                :title="link.name"
-                :alt="link.name"
-                class="is-link"
-                :class="false ? 'is-active' : ''"
-              >
-                {{ link.name }}
-              </a>
-            </template>
-          </li>
-        </ul>
-        <slot name="custom"></slot>
-        <slot name="buttons">
-          <div v-if="isAuth">
-            <a
-              href="javascript:void(0)"
-              @click="logout()"
-              class="btn neutral btn-defaults"
-              >{{ $t('navbar_logout_cta') }}</a
-            >
-            <router-link
-              v-if="showDashboardLink"
-              to="/user"
-              class="btn primary btn-defaults"
-              >{{ $t('navbar_dashboard_cta') }}</router-link
-            >
-          </div>
-          <div v-else>
-            <router-link to="/login" class="btn neutral btn-defaults">{{
-              $t('navbar_login_cta')
-            }}</router-link>
-            <router-link to="/login" class="btn primary btn-defaults">{{
-              $t('navbar_signup_cta')
-            }}</router-link>
-          </div>
+  <HeaderNavbar
+    :title="title"
+    :darkLight="darkLight"
+    :links="links"
+    :showTitle="showTitle"
+  >
+    <template v-slot:logo>
+      <slot name="logo"></slot>
+      <span v-if="title && showTitle">{{ title }}</span>
+    </template>
+    <template v-slot:actions>
+      <ClientOnly>
+        <slot name="cart">
+          <template v-if="showCart">
+            <router-link :to="cartPath" class="nav-cart">
+              <ShoppingCartIcon />
+              <div class="badge">{{ cartCount.toString() }}</div>
+            </router-link>
+          </template>
         </slot>
-      </div>
-    </div>
-  </nav>
+      </ClientOnly>
+    </template>
+    <template v-slot:custom>
+      <slot name="custom"></slot>
+    </template>
+    <template v-slot:buttons>
+      <slot name="buttons">
+        <template v-if="isAuth">
+          <a
+            href="javascript:void(0)"
+            @click="logout()"
+            class="btn neutral btn-defaults"
+            >{{ $t('navbar_logout_cta') }}</a
+          >
+          <router-link
+            v-if="showDashboardLink"
+            to="/user"
+            class="btn primary btn-defaults"
+            >{{ $t('navbar_dashboard_cta') }}</router-link
+          >
+        </template>
+        <template v-else>
+          <router-link to="/login" class="btn neutral btn-defaults">{{
+            $t('navbar_login_cta')
+          }}</router-link>
+          <router-link to="/login" class="btn primary btn-defaults">{{
+            $t('navbar_signup_cta')
+          }}</router-link>
+        </template>
+      </slot>
+    </template>
+  </HeaderNavbar>
 </template>
--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -99,51 +99,44 @@ useFyHead({
         <img src="@/assets/fyvue.svg" class="h-10" />
       </template>
       <template v-slot:custom>
-        <div
-          class="search relative !mb-4 !w-full md:!mb-0 md:!w-auto mx-0 md:mx-3"
-          ref="searchContainer"
-        >
-          <div class="block md:flex items-center">
-            <div class="relative md:block">
-              <form
-                class="flex flex-nowrap w-full"
-                @submit.prevent="
-                  () => {
-                    if (query) $router.push(`/search/${query}`);
-                    else {
-                      $router.push(`/search/`);
-                    }
-                  }
-                "
-              >
-                <FyInput
-                  id="search"
-                  ref="searchInputRef"
-                  v-model="query"
-                  type="search"
-                  placeholder="Search..."
-                >
-                  <template v-slot:before>
-                    <div class="flex items-center px-2">
-                      <svg
-                        class="w-5 h-5 text-fv-neutral-500"
-                        aria-hidden="true"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                    </div>
-                  </template>
-                </FyInput>
-              </form>
-            </div>
-          </div>
+        <div class="fyui-nav__search" ref="searchContainer">
+          <form
+            class=""
+            @submit.prevent="
+              () => {
+                if (query) $router.push(`/search/${query}`);
+                else {
+                  $router.push(`/search/`);
+                }
+              }
+            "
+          >
+            <FyInput
+              id="search"
+              ref="searchInputRef"
+              v-model="query"
+              type="search"
+              placeholder="Search..."
+            >
+              <template v-slot:before>
+                <div class="fyuid-nav__search-icon">
+                  <svg
+                    class="w-5 h-5 text-fv-neutral-500"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+              </template>
+            </FyInput>
+          </form>
         </div>
       </template>
     </FyNavbar>
